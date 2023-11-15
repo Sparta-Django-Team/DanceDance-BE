29dae45f1a30327818a3db85ca44b80117439607
unknown <jvidorca@gmail.com>
2023-11-15 11:10:50 +0900

    Feat: Challenge 앱 View swagger 추가 #6,#7,#8,#10,#12,#13
    - Challenge 앱 내 views.py 코드에 swagger_auto_schema 추가

--- dance_dance/challenges/views.py
+++ dance_dance/challenges/views.py
@@ -1,8 +1,10 @@
 import json
 
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 
 from dance_dance.challenges.functions import download_video
@@ -21,8 +23,16 @@ class VideoListPagination(PageNumberPagination):
 
 
 class VideoListView(PaginationHandlerMixin, APIView):
+    permission_classes = [AllowAny]
     pagination_class = VideoListPagination
 
+    @swagger_auto_schema(
+        tags=["User Video List"],
+        operation_summary="유저 영상 리스트 조회",
+        responses={
+            status.HTTP_200_OK: UserVideoSerializer,
+        },
+    )
     def get(self, request):
         user_videos = UserVideo.objects.all()
         print(user_videos)
@@ -32,6 +42,15 @@ class VideoListView(PaginationHandlerMixin, APIView):
 
 
 class VideoDetailView(APIView):
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        tags=["User Video Detail"],
+        operation_summary="유저 영상 상세페이지 조회",
+        responses={
+            status.HTTP_200_OK: UserVideoSerializer,
+        },
+    )
     def get(self, request, video_id):
         video = get_object_or_404(UserVideo, id=video_id)
         serializer = UserVideoSerializer(video)
@@ -39,17 +58,29 @@ class VideoDetailView(APIView):
 
 
 class VideoLikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        tags=["User Video Like"],
+        operation_summary="유저 영상 좋아요",
+    )
     def post(self, request, video_id):
         video = get_object_or_404(UserVideo, id=video_id)
         if request.user in video.likes.all():
             video.likes.remove(request.user)
-            return create_response("좋아요를 취소했습니다.", status_code=status.HTTP_200_OK)
+            return create_response("unlike", status_code=status.HTTP_200_OK)
         else:
             video.likes.add(request.user)
-            return create_response("좋아요 했습니다.", status_code=status.HTTP_200_OK)
+            return create_response("like", status_code=status.HTTP_200_OK)
 
 
 class VideoLoadView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        tags=["Video Processing"],
+        operation_summary="비디오 로드 및 Score 추출",
+    )
     def post(self, request, file_type):  # file_type은 origin or user만 사용하도록 선택하게끔 한다
         if request.method == "POST":
             data = json.loads(request.body)
@@ -68,6 +99,12 @@ class VideoLoadView(APIView):
 
 
 class TagCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        tags=["Tag Create"],
+        operation_summary="태그 생성",
+    )
     def post(self, request):
         serializer = TagCreateSerializer(data=request.data)
         if serializer.is_valid():
@@ -75,14 +112,3 @@ class TagCreateView(APIView):
             return create_response(serializer.data, status_code=status.HTTP_200_OK)
         else:
             return create_response(serializer.errors, status_code=status.HTTP_400_BAD_REQUEST)
-
-
-class VideoLikeView(APIView):
-    def post(self, request, video_id):
-        video = get_object_or_404(UserVideo, id=video_id)
-        if request.user in video.likes.all():
-            video.likes.remove(request.user)
-            return create_response("unlike", status_code=status.HTTP_200_OK)
-        else:
-            video.likes.add(request.user)
-            return create_response("like", status_code=status.HTTP_200_OK)
